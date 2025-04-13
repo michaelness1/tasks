f893951e680ff6271e89f3ae0a5ec0d528f2a4ae
Michael Ness <michael.r.ness11@gmail.com>
2025-04-13 11:23:33 -0400

    Completed HTML and CSS task

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,51 @@
 import React from "react";
+import { Button, Container, Row, Col } from "react-bootstrap";
 import "./App.css";
 
 function App(): React.JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                UM COS420 with React Hooks and TypeScript
+                <h1>Welcome to My App</h1>
+                <img
+                    src="https://placehold.co/200x200"
+                    alt="A placeholder image"
+                />
+                <ul>
+                    <li>First item</li>
+                    <li>Second item</li>
+                    <li>Third item</li>
+                </ul>
+                <Button
+                    onClick={() => {
+                        console.log("Hello World!");
+                    }}
+                >
+                    Log Hello World
+                </Button>
+                <Container>
+                    <Row>
+                        <Col>
+                            <div
+                                style={{
+                                    width: "100px",
+                                    height: "100px",
+                                    backgroundColor: "red",
+                                }}
+                            ></div>
+                        </Col>
+                        <Col>
+                            <div
+                                style={{
+                                    width: "100px",
+                                    height: "100px",
+                                    backgroundColor: "red",
+                                }}
+                            ></div>
+                        </Col>
+                    </Row>
+                </Container>
             </header>
-            <p>
-                Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload.
-            </p>
         </div>
     );
 }
